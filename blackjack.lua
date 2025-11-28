f5728faa8e0ab4fb2f1c96530f064ba13601165c
pot8todev <pedrohjesus234@gmail.com>
2025-11-27 21:20:43 -0300

    jogo funcionando, pronto para ser entrege

--- blackjack.lua
+++ blackjack.lua
@@ -11,32 +11,60 @@ local eu = jogadores.newPcharacter()
 local bot = jogadores.newNPcharacter("bot")
 local mesa = jogadores.newNPcharacter("mesa")
 
---setup blackjack
-eu:draw(2)
-
+local function setup(prompt, totalMoney)
+	eu:draw(2)
+	bot:draw(2)
+	bot.mao.cartas[2]:flip()
+	os.execute("clear")
+	io.write(GREEN .. string.format("\nvoce possui ") .. RESET)
+	io.write(RED .. string.format("%d dinheiros", eu.dinheiro) .. RESET)
+	io.write(GREEN .. string.format(" mas a casa sempre vence....") .. RESET)
+	while true do
+		io.write(prompt)
+		local input = io.read("*l")
+		local n = tonumber(input)
+		if n ~= nil and n <= totalMoney and n > 0 then
+			return n
+		end
+		print("Entrada inválida, digite um valor que consiga pagar.")
+	end
+end
+eu.dinheiro = 100
 local compra = true
 local loop = true
+local payback = 1.5
+
 while loop do
+	local investimento = setup("\nQuanto deseja investir(juros: " .. payback .. "):", eu.dinheiro)
+	eu.dinheiro = eu.dinheiro - investimento
+
+	local botWin = false
+	local playerWin = false
+
 	while compra == true do
 		os.execute("clear")
+		io.write(string.format("\nvoce possui "))
+		io.write(GREEN .. string.format("%d dinheiros", eu.dinheiro) .. RESET)
+		io.write(string.format("restantes"))
 		if eu.pontos_mao > 21 then
 			local A_index = eu.mao:findCardIndex("A", nil, 11)
 			if A_index then
 				eu.mao.cartas[A_index].pontuacao = 1
 				eu:contaPontos()
-				io.write(string.format(GREEN .. "PREVENT LOSING: A POINTS set to 1 \n" .. RESET))
+				io.write(string.format(GREEN .. "\nPREVENT LOSING: A POINTS set to 1 \n" .. RESET))
 			end
 		end
-		print("player turn, it has " .. eu.pontos_mao .. " pontos")
-		print("bot has, " .. bot.pontos_mao .. " pontos")
+		print("\nplayer turn, it has " .. RED .. eu.pontos_mao .. RESET .. " pontos")
+		print("bot has, " .. RED .. "??" .. RESET .. " pontos")
 		bot:showHand()
 		eu:showHand()
 		if eu.pontos_mao > 21 then
-			loop = false
-			compra = false
-			io.write(string.format(RED .. "YOU ARE OUT" .. RESET))
+			io.write(string.format(RED .. "\nYOU ARE OUT" .. RESET))
+			botWin = true
+
 			break
 		end
+		io.write(string.format("\nvoce deseja comprar outra carta?"))
 		compra = eu.decisao()
 		if compra == true then
 			eu:draw()
@@ -44,31 +72,31 @@ while loop do
 	end
 
 	compra = true
-	while compra == true and loop do
+	bot.mao.cartas[2]:flip(2)
+	while compra == true and not botWin do
 		os.execute("clear")
 		if bot.pontos_mao > 21 then --
 			local A_index = bot.mao:findCardIndex("A", nil, 11)
 			if A_index then
 				bot.mao.cartas[A_index].pontuacao = 1
 				bot:contaPontos()
-				io.write(string.format(GREEN .. "PREVENT LOSING: A POINTS set to 1 \n" .. RESET))
+				io.write(string.format(GREEN .. "\nPREVENT LOSING: A POINTS set to 1 \n" .. RESET))
 			end
 		end
-		print("player has, " .. eu.pontos_mao .. " pontos")
-		print("bot turn, it has , " .. bot.pontos_mao .. " pontos")
+		print("\nplayer has, " .. RED .. eu.pontos_mao .. RESET .. " pontos")
+		print("bot turn, it has " .. RED .. bot.pontos_mao .. RESET .. " pontos")
 		eu:showHand()
 		bot:showHand() -- inverte as maos
 		print()
-		local _ = io.write() --useless input
+		local _ = io.read() --useless input
 
 		if bot.pontos_mao > 21 then
-			loop = false
-			compra = false
-			io.write(string.format(RED .. "YOU ARE OUT" .. RESET))
+			playerWin = true
+
+			io.write(string.format(RED .. "\nBOT IS OUT\n" .. RESET))
 			break
 		elseif compra == false then
-			loop = false
-			io.write(string.format(RED .. "the house allways wins" .. RESET))
+			io.write(string.format(RED .. "\nthe house allways wins" .. RESET))
 			break
 		end
 		compra = bot.decisao(bot.pontos_mao, eu.pontos_mao)
@@ -76,6 +104,29 @@ while loop do
 			bot:draw()
 		end
 	end
-	print("end of game")
-	loop = false
+	if botWin then
+		os.execute("clear")
+		print("\nplayer has, " .. RED .. eu.pontos_mao .. RESET .. " pontos")
+		print("bot has " .. RED .. bot.pontos_mao .. RESET .. " pontos")
+		print("house allways wins")
+		loop = eu:decisao()
+	elseif playerWin then
+		io.write(
+			GREEN
+				.. string.format("\ninvestiu %.2f voce ganhow %.2f dinheiro", investimento, investimento * payback)
+				.. RESET
+		)
+		investimento = investimento * payback
+		payback = payback * payback
+		io.write(GREEN .. string.format("\njuros de vitoria aumentado: %.2f", payback) .. RESET)
+		io.write(string.format("\ndeseja continuar?"))
+		eu.dinheiro = eu.dinheiro + investimento
+		loop = eu:decisao()
+	end
+	if eu.dinheiro <= 0 then
+		print(RED .. "voce tem problemas." .. RESET)
+		loop = false
+	end
+	eu:discardHand()
+	bot:discardHand()
 end
